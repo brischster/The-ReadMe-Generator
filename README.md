7acf6490d4d252dc4ff94b6571a1af48ac751e02
Ryan Brisch <brischster@gmail.com>
2022-05-26 22:08:42 -0600

    got all content generation and markdown styling done

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-{"title":"test","description":"Apache License 2.0","instructions":"download","usage":"use it","contribution":"chip in","tests":"test it","github":"git","email":"m@me.com"}
+# <ins>best ever</ins>
+
+## <ins>Table of Contents
+
+1. [Description](#Description)
+2. [Installation Instructions](#How_to_Install)
+3. [How to Use](#How_to_Use)
+4. [How to Contribute](#How_to_Contribute)
+5. [Tests](#Test_Cases)
+6. [Badges and License](#Badges_and_License)
+7. [Credits/Collaborators](#Credits_and_Collaborators)
+8. [Contact Information](#Author_Contact_Information)
+
+### <ins>Description <a name="Description"></a></ins></br>
+
+the best thing ever
+
+### <ins>How to Install <a name="How_to_Install"></a></ins></br>
+
+click download
+
+### <ins>How to Use <a name="How_to_Use"></a></ins></br>
+
+enter the information
+
+### <ins>How to Contribute <a name="How_to_Install"></a></ins>
+
+[Code of Conduct Expected by All Contributors](https://www.contributor-covenant.org/version/2/1/code_of_conduct/)
+
+be nice
+
+### <ins>Tests Cases <a name="Test_Cases"></a></ins>
+
+tried and true
+
+### <ins>Badges and License<a name="Badges_and_License"></a></ins>
+
+Apache License 2.0
+
+[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-2.1-4baaaa.svg)](code_of_conduct.md)
+
+### <ins>Credits and Collaborators<a name="Credits_and_Collaborators"></a></ins>
+
+medium1
+
+### <ins>Author's Contact Information<a name="Author_Contact_Information"></a></ins>
+
+Ryan Brisch
+[Github Profile](github.com)
+[Email Ryan](mailto:brischster@gmail.com)
--- index.js
+++ index.js
@@ -50,11 +50,22 @@ inquirer
       type: "rawlist",
       message: "Please select a license for your project",
       choices: ["Apache License 2.0", "MIT", "GNV GPLv3", "ISC"],
-      name: "description",
+      name: "license",
     },
     {
       type: "input",
-      message: "Please provide a link for your Github profile",
+      message:
+        "Please provide a names of any contributors and/or other sources that need to be credited",
+      name: "credits",
+    },
+    {
+      type: "input",
+      message: "Please provide author's full name",
+      name: "author",
+    },
+    {
+      type: "input",
+      message: "Please provide the link to your Github Profile",
       name: "github",
     },
     {
@@ -64,8 +75,49 @@ inquirer
     },
   ])
   .then((response) => {
-    const allPageInfo = ``;
-    fs.writeFile("README.md", JSON.stringify(response), (err) => {
+    const allPageInfo = `<ins> #${response.title}</ins>
+
+## Table of Contents
+
+1. [Description](#Description)
+2. [Installation Instructions](#How_to_Install)
+3. [How to Use](#How_to_Use)
+4. [How to Contribute](#How_to_Contribute)
+5. [Tests](#Test_Cases)
+6. [Badges and License](#Badges_and_License)
+7. [Credits/Collaborators](#Credits_and_Collaborators)
+8. [Contact Information](#Author_Contact_Information)
+
+<ins>### Description <a name="Description"></a></ins>
+${response.description}
+
+<ins>### How to Install <a name="How_to_Install"></a></ins>
+${response.instructions}
+
+<ins>### How to Use <a name="How_to_Use"></a></ins>
+${response.usage}
+
+<ins>### How to Contribute <a name="How_to_Install"></a></ins>
+[Code of Conduct Expected by All Contributors](https://www.contributor-covenant.org/version/2/1/code_of_conduct/)
+
+${response.contribution}
+
+<ins>### Tests Cases <a name="Test_Cases"></a></ins>
+${response.tests}
+
+<ins>### Badges and License<a name="Badges_and_License"></a></ins>
+${response.license}
+
+[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-2.1-4baaaa.svg)](code_of_conduct.md)
+
+<ins>### Credits and Collaborators<a name="Credits_and_Collaborators"></a></ins>
+${response.credits}
+
+<ins>### Author's Contact Information<a name="Author_Contact_Information"></a></ins>
+${response.author}
+[Github Profile](${response.github})
+[Email Ryan](mailto:${response.email})`;
+    fs.writeFile("README.md", allPageInfo, (err) => {
       err
         ? console.log(err)
         : console.log("Responses have been loaded to README file");
